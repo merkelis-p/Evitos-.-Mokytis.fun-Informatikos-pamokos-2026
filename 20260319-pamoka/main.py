c51f5752eb693ea7b4f387de8d424df36d43b8b4
Patrikas Merkelis <merkelis-p@Patrikass-MacBook-Pro.local>
2026-03-26 13:42:53 +0200

    Initial commit

---
+++ 20260319-pamoka/main.py
@@ -0,0 +1,31 @@
+# x = 2
+
+# print(x)
+
+# x = 3
+
+# print(x)
+
+mas = [1, 2, 3, 4, 5]
+
+print(len(mas))
+
+x = 10
+
+print(x)
+
+x = x + 5
+
+print(x)
+
+
+x += 5
+
+print(x)
+
+
+def stastika(mas, n):
+    return 1.349549584895
+
+
+print(f"{stastika(1,2):.2f} stastika")
