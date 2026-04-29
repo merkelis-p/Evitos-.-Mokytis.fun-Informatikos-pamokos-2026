5daa42b8ba9529cf2a93d1ccd84cc8fdabccfdfb
Evita <evita200807@gmail.com>
2026-04-29 11:40:14 -0700

    Praeitu pamoku namu darbai

--- 20260409-20260416-pamoka/u2.py
+++ 20260409-20260416-pamoka/u2.py
@@ -4,3 +4,18 @@
 # Reikia naudoti: for, continue
 
 # Tavo kodas žemiau
+n = int(input())
+
+suma = 0
+praleista = 0
+
+for _ in range(n):
+    skaicius = int(input())
+    
+    if skaicius <= 0:
+        praleista += 1
+        continue
+    
+    suma += skaicius
+
+print(suma, praleista)
